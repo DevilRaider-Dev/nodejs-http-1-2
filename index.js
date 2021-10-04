5452d9b41b7079eab9ea49bc49d2b42adb5d7641
DevilRaider-Dev <deviltwix3@gmail.com>
2021-10-04 13:47:19 +0200

    nodejs http lession 1&2

---
+++ index.js
@@ -0,0 +1,35 @@
+// Level 1-1
+
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+    let filePath = req.url === '/' ? '/index.html' : req.url
+    let ext = path.extname(req.url)
+    console.log(ext)
+    let contentType = 'text/html'
+    switch (ext) {
+        // Level 2-1
+        case '.css':
+            contentType = 'text/css'
+            break;
+        case '.js':
+            contentType = 'text/javascript'
+            break;
+        case '.ico':
+            contentType = 'image/x-icon'
+            break;
+    }
+    console.log(contentType)
+
+    fs.readFile(`./public${filePath}`, (err, data) => {
+        if (err) throw err
+        res.writeHead(200, { 'Content-Type': contentType })
+        res.end(data)
+    })
+})
+
+server.listen(3000, () => {
+    console.log('listening at 127:0:0:1:3000')
+})
